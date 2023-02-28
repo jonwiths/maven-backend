0a8f864cba15f408e6944c049a9324ae11c93c6c
Jonslogar Arenque <jonwiths@gmail.com>
2023-02-28 23:28:24 +0800

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['https://maven-edu.netlify.app/'],
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
   })
