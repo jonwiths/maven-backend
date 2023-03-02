d350c5e26d8566e8f1bbe9324ec211e2f7bdf70e
Jonslogar Arenque <jonwiths@gmail.com>
2023-03-02 12:52:49 +0800

    make it better

--- connection/connect.js
+++ connection/connect.js
@@ -1,3 +1,14 @@
+// const mysql = require('mysql');
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE
+// });
+
+// module.exports = { db };
+
 const mysql = require('mysql');
 
 const db = mysql.createPool({
@@ -5,6 +16,20 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
+});
+
+// Add error event listener to handle connection errors
+db.on('error', function (err) {
+  console.error('MySQL connection error:', err.message);
+  // Attempt to reconnect to the database
+  db.getConnection(function (err, connection) {
+    if (err) {
+      console.error('Error reconnecting to the database:', err.message);
+    } else {
+      console.log('Successfully reconnected to the database');
+      connection.release();
+    }
+  });
 });
 
 module.exports = { db };
