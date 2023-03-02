175a53ead7f9153835ee8310e4c5fcd92b3b8a39
Jonslogar Arenque <jonwiths@gmail.com>
2023-03-02 13:35:31 +0800

    make it better

--- controllers/student/getStudentHistory.js
+++ controllers/student/getStudentHistory.js
@@ -11,14 +11,16 @@ const getStudentHistory = (req, res) => {
     req.headers['authorization'] ||
     req.cookies.user;
 
+  const history_id = req.body.history_id;
+
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, userInfo) => {
     if (err) {
       return res.status(500).json(err);
     } else {
       const q =
-        'SELECT H.*, CT.duration AS duration, CT.start AS start, CT.end AS end, CT.date AS date, M.id AS m_id,  M.f_name AS m_fname, M.subject AS subject, M.l_name AS m_lname FROM `heroku_064c14c6215e460`.history AS H JOIN `heroku_064c14c6215e460`.students AS S ON H.student_id = S.id JOIN `heroku_064c14c6215e460`.create_timings AS CT ON H.create_timing_id = CT.id JOIN `heroku_064c14c6215e460`.mentors AS M ON H.mentor_id = M.id WHERE H.student_id = ?;';
+        'SELECT H.*, CT.duration AS duration, CT.start AS start, CT.end AS end, CT.date AS date, M.id AS m_id,  M.f_name AS m_fname, M.subject AS subject, M.l_name AS m_lname FROM `heroku_064c14c6215e460`.history AS H JOIN `heroku_064c14c6215e460`.students AS S ON H.student_id = S.id JOIN `heroku_064c14c6215e460`.create_timings AS CT ON H.create_timing_id = CT.id JOIN `heroku_064c14c6215e460`.mentors AS M ON H.mentor_id = M.id WHERE AND H.id = ?;';
 
-      db.query(q, [userInfo.id], (err, data) => {
+      db.query(q, [history_id], (err, data) => {
         if (err) return res.status(500).json(err);
         else {
           res.status(200).json(data);
